0020e75de05b84e8eea9ddfe36cd9c6713b924bc
Md. Samin Israk 2021362642 <samin.israk@northsouth.edu>
2025-08-03 20:00:24 +0600

    problem 4 solved

--- final.js
+++ final.js
@@ -29,3 +29,15 @@ for (var i=1; i<=lastDay; i++){
   console.log(i + " - rest")
 }
 
+/** Problem 04 - (Delete / Store) */
+var fileName= "#exp.mp4";
+//write your code here
+
+var splited = fileName.split(".");
+
+if (splited[1] === "pdf" || splited[1] === "docx" || fileName[0] === "#") {
+  console.log("Store");
+} else {
+  console.log("Delete");
+}
+
